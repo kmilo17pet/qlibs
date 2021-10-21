776f6f386c11a4b9173de29c6c2e252f5b6fb9e3
kmilo17pet <kmilo17pet@gmail.com>
2021-10-21 09:53:16 -0500

    fixes and improvements to qbitfield

--- include/qbitfield.h
+++ include/qbitfield.h
@@ -1,7 +1,7 @@
 /*!
  * @file qbitfield.h
  * @author J. Camilo Gomez C.
- * @version 1.05
+ * @version 1.06
  * @note This file is part of the qTools distribution.
  * @brief A bit-field manipulation library.
  **/
@@ -37,115 +37,115 @@ extern "C" {
 
     /**
     * @brief Setup a initialize a BitField instance.
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] area  A pointer to the memory block to hold the BitField. 
     * Should be an uint8_t array of size #BITFIELD_SIZE(n), where n, is the 
     * number of bits inside the BitField.
     * @param[in] area_size The number of bytes in @a area.
     * @return 1 on success, otherwise return 0.
     */ 
-    int qBitField_Setup( qBitField_t *instance, void *area, size_t area_size );
+    int qBitField_Setup( qBitField_t *b, void *area, const size_t area_size );
 
     /**
     * @brief Clear all the bits in the BitField.
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @return 1 on success, otherwise return 0.
     */     
-    int qBitField_ClearAll( qBitField_t *instance );
+    int qBitField_ClearAll( qBitField_t *b );
 
     /**
     * @brief Set all the bits in the BitField.
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @return 1 on success, otherwise return 0.
     */       
-    int qBitField_SetAll( qBitField_t *instance );
+    int qBitField_SetAll( qBitField_t *b );
 
     /**
     * @brief Sets one bit in a BitField
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] index The bit-index.
     * @return 1 on success, otherwise return 0.
     */       
-    int qBitField_SetBit( qBitField_t *instance, size_t index );
+    int qBitField_SetBit( qBitField_t *b, const size_t index );
 
     /**
     * @brief Clears one bit in a BitField
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] index The bit-index.
     * @return 1 on success, otherwise return 0.
     */        
-    int qBitField_ClearBit( qBitField_t *instance, size_t index );
+    int qBitField_ClearBit( qBitField_t *b, const size_t index );
 
     /**
     * @brief Toggles (i.e. reverses the state of) a bit in a BitField
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] index The bit-index.
     * @return 1 on success, otherwise return 0.
     */     
-    int qBitField_ToggleBit( qBitField_t *instance, size_t index );
+    int qBitField_ToggleBit( qBitField_t *b, const size_t index );
 
     /**
     * @brief Retrieve the state of a bit in a bitfield
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] index The bit-index.
     * @return The value of the bit at @a index.
     */      
-    uint8_t qBitField_ReadBit( const qBitField_t *instance, size_t index );
+    uint8_t qBitField_ReadBit( const qBitField_t *b, const size_t index );
 
     /**
     * @brief Writes one bit in a bitfield
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] index The bit-index.
     * @param[in] value The boolean value to write.
     * @return 1 on success, otherwise return 0.
     */         
-    int qBitField_WriteBit( qBitField_t *instance, size_t index, uint8_t value );
+    int qBitField_WriteBit( qBitField_t *b, const size_t index, uint8_t value );
 
     /**
     * @brief Reads an unsigned 32-bit value from the BitField
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] index  The bit-index taken as offset.
     * @param[in] xBits The number of bits to read. ( max allowed : 32 bits )
     * @return The value from the bitfield from the desired index
     */     
-    uint32_t qBitField_ReadUINTn( qBitField_t *instance, size_t index, size_t xBits );
+    uint32_t qBitField_ReadUINTn( const qBitField_t *b, const size_t index, size_t xBits );
 
     /**
     * @brief Writes an unsigned n-bit value from the BitField
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] index  The bit-index taken as offset.
     * @param[in] value The value to write.
     * @param[in] xBits The number of bits to read. ( max allowed : 32 bits )
     * @return 1 on success, otherwise return 0.
     */         
-    int qBitField_WriteUINTn( qBitField_t *instance, size_t index, uint32_t value, size_t xBits );
+    int qBitField_WriteUINTn( qBitField_t *b, const size_t index, uint32_t value, size_t xBits );
 
     /**
     * @brief Reads a 32-bit floating point value from the BitField
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] index  The bit-index taken as offset.
     * @return The floating point value from the BitField at the desired index
     */       
-    float qBitField_ReadFloat( const qBitField_t *instance, size_t index );
+    float qBitField_ReadFloat( const qBitField_t *b, const size_t index );
 
     /**
     * @brief Writes a 32-bit floating point value to the BitField
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] index  The bit-index taken as offset.
     * @param[in] value The floating point value to write.
     * @return 1 on success, otherwise return 0.
     */       
-    int qBitField_WriteFloat( qBitField_t *instance, size_t index, float value );
+    int qBitField_WriteFloat( qBitField_t *b, const size_t index, float value );
 
     /**
     * @brief Copies @a n bytes from the bit-field instance to a designed memory area.
-    * @param[in] instance A pointer to the BitField instance
+    * @param[in] b A pointer to the BitField instance
     * @param[in] dst  Pointer to the destination array where the content is to
     * be copied.
     * @param[in] n Number of bytes to copy.
     * @return Destination is returned on success, otherwise NULL.
     */       
-    void* qBitField_Dump( qBitField_t *instance, void* dst, size_t n );
+    void* qBitField_Dump( const qBitField_t *b, void* dst, size_t n );
 
 #ifdef __cplusplus
 }
--- qbitfield.c
+++ qbitfield.c
@@ -5,228 +5,268 @@
  **/
 #include "qbitfield.h"
 
+static const size_t qBitField_LBit = (size_t)( sizeof(uint32_t) * 8uL );
+static uint32_t qBitField_Mask( const size_t index );
+static size_t qBitField_BitSlot( const size_t index );
+static uint32_t qBitField_BitGet( const qBitField_t *b, const size_t index );
+static void qBitField_BitSet( qBitField_t *b, size_t index );
+static void qBitField_BitClear( qBitField_t *b, const size_t index );
+static void qBitField_BitToggle( qBitField_t *b, const size_t index );
+static uint32_t qBitField_SafeMask( const uint32_t val, const size_t x, const size_t nbits );
+static size_t qBitField_Offset( const size_t index );
 
-#define LONG_BIT                ( (size_t)(sizeof(uint32_t) * 8uL ) )
-#define BITMASK(b)              ( (uint32_t)1uL << ( (b) % LONG_BIT ) )
-#define BITSLOT(b)              ( (b) / LONG_BIT ) /*((b) >> 6 )*/
-#define BITGET(a, b)            ( ( (a)->field[ BITSLOT(b) ] >> ( (b) % LONG_BIT ) ) & 1uL )
-#define BITSET(a, b)            ( (a)->field[ BITSLOT(b) ] |= BITMASK(b) )
-#define BITCLEAR(a, b)          ( (a)->field[ BITSLOT(b) ] &= ~BITMASK(b) )
-#define BITTEST(a, b)           ( (a)->field[ BITSLOT(b) ] & BITMASK(b) )
-#define BITTOGGLE(a, b)         ( (a)->field[ BITSLOT(b) ] ^= BITMASK(b) )
-#define BITNSLOTS(nb)           ( ( nb + LONG_BIT - 1 ) / LONG_BIT )
-#define BITOFFSET(index)        ( (index) & (uint32_t)31u )
-#define BITMASKMERGE(a,b,abits) ( (b) ^ ( ( (a) ^ (b) ) & (abits) ) )
-#define BITMASK32(nbits)        ( ( 0u != ( nbits) )? ( ~(uint32_t)0 >> ( ( sizeof(uint32_t)*8uL ) - (nbits) ) ) : (uint32_t)0 ) 
-
-
-static uint32_t qBitField_Read_uint32( const qBitField_t *instance, size_t index );
-static void qBitField_Write_uint32( qBitField_t *instance, size_t index, uint32_t value );
+static uint32_t qBitField_Read_uint32( const qBitField_t *instance, const size_t index );
+static void qBitField_Write_uint32( qBitField_t *instance, const size_t index, const uint32_t value );
 
 /*============================================================================*/
-int qBitField_Setup( qBitField_t *instance, void *area, size_t area_size )
+static uint32_t qBitField_Mask( const size_t index )
+{
+    return (uint32_t)1uL << ( index % qBitField_LBit );
+}
+/*============================================================================*/
+static size_t qBitField_BitSlot( const size_t index )
+{
+    return index/qBitField_LBit;
+}
+/*============================================================================*/
+static uint32_t qBitField_BitGet( const qBitField_t *b, const size_t index )
+{
+    const size_t slot = qBitField_BitSlot( index );
+    return  ( b->field[ slot ] >> ( index % qBitField_LBit ) ) & 1uL;
+}
+/*============================================================================*/
+static void qBitField_BitSet( qBitField_t *b, size_t index )
+{
+    const size_t slot = qBitField_BitSlot( index );
+    b->field[ slot ] |= qBitField_Mask( index );
+}
+/*============================================================================*/
+static void qBitField_BitClear( qBitField_t *b, const size_t index )
+{
+    const size_t slot = qBitField_BitSlot( index );
+    b->field[ slot ] &= ~qBitField_Mask( index );
+}
+/*============================================================================*/
+static void qBitField_BitToggle( qBitField_t *b, const size_t index )
+{
+    const size_t slot = qBitField_BitSlot( index );
+    b->field[ slot ] ^= qBitField_Mask( index );
+}
+/*============================================================================*/
+static uint32_t qBitField_SafeMask( const uint32_t val, const size_t x, const size_t nbits )
+{
+    /*cstat -ATH-shift-bounds -MISRAC2012-Rule-12.2 -CERT-INT34-C_b*/
+    return val >> ( (uint32_t)( x - nbits ) );
+    /*cstat +ATH-shift-bounds -MISRAC2012-Rule-12.2 +CERT-INT34-C_b*/
+}
+/*============================================================================*/
+static size_t qBitField_Offset( const size_t index )
+{
+    return index & (size_t)31u;
+}
+/*============================================================================*/
+static uint32_t qBitField_MaskMerge( const uint32_t w, const uint32_t value, const uint32_t mask )
+{
+    return value ^ ( ( w ^ value ) & mask );
+}
+/*============================================================================*/
+int qBitField_Setup( qBitField_t *b, void *area, const size_t area_size )
 {
     int retValue = 0;
-    if ( ( NULL != instance ) && ( NULL != area ) && ( area_size > 0u ) ) {
+    if ( ( NULL != b ) && ( NULL != area ) && ( area_size > 0u ) ) {
         /*cstat -MISRAC2012-Rule-11.5 -CERT-EXP36-C_b*/
-        instance->field = (uint32_t *)area;
+        b->field = (uint32_t *)area;
         /*cstat +MISRAC2012-Rule-11.5 +CERT-EXP36-C_b*/
-        instance->size = area_size*8u;
-        instance->nSlots = area_size/sizeof(uint32_t);      
+        b->size = area_size*8u;
+        b->nSlots = area_size/sizeof(uint32_t);      
         retValue = 1; 
     }
     return retValue;
 }
 /*============================================================================*/
-int qBitField_ClearAll( qBitField_t *instance )
+int qBitField_ClearAll( qBitField_t *b )
 {
     int retValue = 0;
-    if ( NULL != instance ) {
-        (void)memset( instance->field, 0, instance->size/8u );
+    if ( NULL != b ) {
+        (void)memset( b->field, 0, b->size/8u );
         retValue = 1;
     }
     return retValue;
 }
 /*============================================================================*/
-int qBitField_SetAll( qBitField_t *instance )
+int qBitField_SetAll( qBitField_t *b )
 {
     int retValue = 0;
-    if ( NULL != instance ) {
-        (void)memset( instance->field, 0xFF, instance->size/8u );
+    if ( NULL != b ) {
+        (void)memset( b->field, 0xFF, b->size/8u );
         retValue = 1;
     }
     return retValue;
 }
 /*============================================================================*/
-int qBitField_SetBit( qBitField_t *instance, size_t index )
+int qBitField_SetBit( qBitField_t *b, const size_t index )
 {
     int retValue = 0;
-    if ( NULL != instance ) {
-        BITSET( instance, index );
+    if ( NULL != b ) {
+        qBitField_BitSet( b, index );
         retValue = 1;
     }
     return retValue;
 }
 /*============================================================================*/
-int qBitField_ClearBit( qBitField_t *instance, size_t index )
+int qBitField_ClearBit( qBitField_t *b, const size_t index )
 {
     int retValue = 0;
-    if ( NULL != instance ) {
-        BITCLEAR( instance, index );
+    if ( NULL != b ) {
+        qBitField_BitClear( b, index );
         retValue = 1;
     }
     return retValue;
 }
 /*============================================================================*/
-int qBitField_ToggleBit( qBitField_t *instance, size_t index )
+int qBitField_ToggleBit( qBitField_t *b, const size_t index )
 {
     int retValue = 0;
-    if ( NULL != instance ) {
-        BITTOGGLE( instance, index );
+    if ( NULL != b ) {
+        qBitField_BitToggle( b, index );
         retValue = 1;
     }
     return retValue;
 }
 /*============================================================================*/
-uint8_t qBitField_ReadBit( const qBitField_t *instance, size_t index )
+uint8_t qBitField_ReadBit( const qBitField_t *b, const size_t index )
 {
     uint8_t retValue = 0u;
-    if ( NULL != instance ) {
-        retValue = ( 0u != BITGET( instance, index ) )? 1u : 0u;
+    if ( NULL != b ) {
+        retValue = ( 0uL != qBitField_BitGet( b, index ) )? 1u : 0u;
     }
     return retValue;
 }
 /*============================================================================*/
-int qBitField_WriteBit( qBitField_t *instance, size_t index, uint8_t value )
+int qBitField_WriteBit( qBitField_t *b, const size_t index, uint8_t value )
 {
     int retValue = 0;
-    if ( NULL != instance ) {
+    if ( NULL != b ) {
         if ( 0u != value ) {
-            BITSET( instance, index );
+            qBitField_BitSet( b, index );
         }
         else {
-            BITCLEAR( instance, index );
+            qBitField_BitClear( b, index );
         }
         retValue = 1;
     }
     return retValue;
 }
 /*============================================================================*/
-uint32_t qBitField_ReadUINTn( qBitField_t *instance, size_t index, size_t xBits )
+uint32_t qBitField_ReadUINTn( const qBitField_t *b, const size_t index, size_t xBits )
 {
     uint32_t retValue = 0uL;
-    if ( ( NULL != instance ) && ( xBits <= 32u ) ) {
+    if ( ( NULL != b ) && ( xBits <= 32u ) ) {
         if ( 1u == xBits ) {
-            retValue = (uint32_t)qBitField_ReadBit( instance, index );
+            retValue = (uint32_t)qBitField_ReadBit( b, index );
         }
         else if ( 32u == xBits ) {
-            retValue = qBitField_Read_uint32( instance, index );
+            retValue = qBitField_Read_uint32( b, index );
         }
         else {
-            retValue = qBitField_Read_uint32( instance, index );
-            /*cstat -ATH-shift-bounds -MISRAC2012-Rule-12.2 -CERT-INT34-C_b*/
-            retValue &= ( ( ~((uint32_t)0) ) >> ( 32u - xBits ) ); /*safe mask*/ 
-            /*cstat +ATH-shift-bounds +MISRAC2012-Rule-12.2 +CERT-INT34-C_b*/
+            retValue = qBitField_Read_uint32( b, index );
+            retValue &= qBitField_SafeMask( 0xFFFFFFFFuL, 32u, xBits );
         }        
     }
     return retValue;
 }
 /*============================================================================*/
-int qBitField_WriteUINTn( qBitField_t *instance, size_t index, uint32_t value, size_t xBits )
+int qBitField_WriteUINTn( qBitField_t *b, const size_t index, uint32_t value, size_t xBits )
 {
     int retValue = 0;
-    if ( ( NULL != instance ) && ( xBits <= 32u ) ) {
+    if ( ( NULL != b ) && ( xBits <= 32u ) ) {
         uint32_t w, mask;
         if ( 1u == xBits ) {
-            (void)qBitField_WriteBit( instance, index, (uint8_t)value );
+            (void)qBitField_WriteBit( b, index, (uint8_t)value );
         }
         else if ( 32u == xBits ) {
-            qBitField_Write_uint32( instance, index, value );
+            qBitField_Write_uint32( b, index, value );
         }
         else {
-            w = qBitField_Read_uint32( instance, index );
-            /*cstat -ATH-shift-bounds -MISRAC2012-Rule-12.2 -CERT-INT34-C_b*/
-            value &= ( ( ~((uint32_t)0) ) >> ( 32u - xBits ) ); /*safe mask*/ /*ATH-shift-bounds,MISRAC2012-Rule-12.2 deviation allowed*/
-            /*cstat +ATH-shift-bounds +MISRAC2012-Rule-12.2 +CERT-INT34-C_b*/
+            w = qBitField_Read_uint32( b, index );
+            value &= qBitField_SafeMask( 0xFFFFFFFFuL, 32u, xBits );
             /*cstat -ATH-overflow*/
-            mask = ( ~((uint32_t)0) ) << xBits;
+            mask = (uint32_t)0xFFFFFFFFuL << xBits;
             /*cstat +ATH-overflow*/
-            qBitField_Write_uint32( instance, index, BITMASKMERGE( w, value, mask ) );         
+            qBitField_Write_uint32( b, index, qBitField_MaskMerge( w, value, mask ) );         
         }        
         retValue = 1;
     }
     return retValue;
 }
 /*============================================================================*/
-float qBitField_ReadFloat( const qBitField_t *instance, size_t index )
+float qBitField_ReadFloat( const qBitField_t *b, const size_t index )
 {
     float retValue = 0.0f;
-    if ( NULL != instance ) {
+    if ( NULL != b ) {
         uint32_t rval;
-        rval = qBitField_Read_uint32( instance, index );
+        rval = qBitField_Read_uint32( b, index );
         (void)memcpy( &retValue, &rval, sizeof(float) );
     }
     return retValue;
 }
 /*============================================================================*/
-int qBitField_WriteFloat( qBitField_t *instance, size_t index, float value )
+int qBitField_WriteFloat( qBitField_t *b, size_t index, float value )
 {
     int retValue = 0;
-    if ( NULL != instance ) {
+    if ( NULL != b ) {
         uint32_t fval = 0uL;
         (void)memcpy( &fval, &value, sizeof(float) );
-        qBitField_Write_uint32( instance, index, fval );
+        qBitField_Write_uint32( b, index, fval );
         retValue = 1;
     }
     return retValue;
 }
 /*============================================================================*/
-void* qBitField_Dump( qBitField_t *instance, void* dst, size_t n )
+void* qBitField_Dump( const qBitField_t *b, void* dst, size_t n )
 {
     void *retValue = NULL;
-    if ( ( NULL != instance ) && ( NULL != dst ) ) {
-        if ( n <= ( instance->size/8u ) ) {
-            retValue = memcpy( dst, (void*)instance->field, n );
+    if ( ( NULL != b ) && ( NULL != dst ) ) {
+        if ( n <= ( b->size/8u ) ) {
+            retValue = memcpy( dst, (const void*)b->field, n );
         }
     }
     return retValue;
 }
 /*============================================================================*/
-static uint32_t qBitField_Read_uint32( const qBitField_t *instance, size_t index )
+static uint32_t qBitField_Read_uint32( const qBitField_t *b, const size_t index )
 {
-    size_t slot;
-    uint8_t of, bits_taken;
+    size_t slot, of, bits_taken;
     uint32_t result;
 
-    slot = BITSLOT(index);
-    of = (uint8_t)BITOFFSET( index );
-    result = instance->field[ slot ] >> of;
-    bits_taken  = (uint8_t)( 32u - of );
-    
-    if ( ( 0u != of ) && ( ( index + bits_taken ) < instance->size ) ) {
+    slot = qBitField_BitSlot( index );
+    of = qBitField_Offset( index );
+    result = b->field[ slot ] >> of;
+    bits_taken  = 32u - of;
+    if ( ( 0u != of ) && ( ( index + bits_taken ) < b->size ) ) {
         /*cstat -CERT-INT30-C_a -ATH-shift-bounds -MISRAC2012-Rule-12.2 -CERT-INT34-C_b*/
-        result |= instance->field[ slot + 1u ] << (uint32_t)bits_taken ;
+        result |= b->field[ slot + 1u ] << (uint32_t)bits_taken ;
         /*cstat +CERT-INT30-C_a +ATH-shift-bounds +MISRAC2012-Rule-12.2 +CERT-INT34-C_b*/
     }
     return result;
 }
 /*============================================================================*/
-static void qBitField_Write_uint32( qBitField_t *instance, size_t index, uint32_t value )
+static void qBitField_Write_uint32( qBitField_t *b, const size_t index, const uint32_t value )
 {
-    uint8_t of;
     uint32_t mask;
-    size_t slot;
-    slot = BITSLOT( index );
-    of = (uint8_t)BITOFFSET( index );
+    size_t slot, of;
+
+    slot = qBitField_BitSlot( index );
+    of = qBitField_Offset( index );
     if ( 0u == of ) {
-        instance->field[ slot ] = value;
+        b->field[ slot ] = value;
     }
     else {
-        mask = BITMASK32( of );
-        instance->field[ slot   ] = ( value << of ) | ( instance->field[slot] & mask );
-        if ( ( slot+1u ) < instance->nSlots ) {
-            instance->field[ slot+1u ] = ( value >> ( 32u - of ) ) | ( instance->field[ slot+1u ] & ( ~mask ) );  
+        mask = qBitField_SafeMask( 0xFFFFFFFFuL, qBitField_LBit, of );  
+        b->field[ slot ] = ( value << of ) | ( b->field[ slot ] & mask );
+        if ( ++slot < b->nSlots ) {
+            b->field[ slot ] = qBitField_SafeMask( value, 32u, of ) | ( b->field[ slot ] & ( ~mask ) );  
         }
-     }
+    }
 }
 /*============================================================================*/
