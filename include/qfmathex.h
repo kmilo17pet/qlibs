e45153abe5eb3f9a776d8e78be3e230873ae1412
camilo <juancamilo.gomezcadavid@sensiaglobal.com>
2022-11-09 19:42:07 -0500

    updated doc

--- include/qfmathex.h
+++ include/qfmathex.h
@@ -17,6 +17,11 @@ extern "C" {
     #include <stdint.h>
     #include <stdbool.h>
 
+    /** @addtogroup  qfmathex Extra floating-point math
+    * @brief Extra floating-point math and analysis functions
+    *  @{
+    */
+
      /**
     * @brief Scales the given input @a x in value range given by  @a xMin and 
     * @a xMax in value range specified by the @a yMin and @a yMax.
@@ -59,12 +64,32 @@ extern "C" {
     bool qFMathEx_Equal( const float a,
                          const float b );
 
+    /**
+    * @brief Determines if the point at ( @a x, @a y) is inside the polygon given
+    * by the set of points on  @a px and @a py.
+    * @param[in] x Point x-coordinate
+    * @param[in] y Point y-coordinate 
+    * @param[in] px x-coordinate points of the polygon 
+    * @param[in] py y-coordinate points of the polygon 
+    * @param[in] p Number of points that represent the polygon 
+    * @return true when the given point is inside the polygon 
+    */
     bool qFMathEx_InPolygon( const float x,
                              const float y,
                              const float * const px,
                              const float * const py,
                              const size_t p );
 
+    /**
+    * @brief Determines if the point at ( @a x, @a y) is inside the circle 
+    * with radius @a r located at @a cx and @a cy.
+    * @param[in] x Point x-coordinate
+    * @param[in] y Point y-coordinate
+    * @param[in] cx X-location of the circle
+    * @param[in] cy Y-location of the circle
+    * @param[in] r Radio of the circle 
+    * @return true when the given point is inside the circle 
+    */
     bool qFMathEx_isInsideCircle( const float x,
                                   const float y,
                                   const float cx,
