a40579f40923deba94a5f11baaf61008d299163f
kmilo17pet <kmilo17pet@users.noreply.github.com>
2022-11-10 00:35:32 +0000

    deploy: 9ff5563b543c67d692be9a8e28d9ec02cc51f6a9

--- group__qtypegeneric.js
+++ group__qtypegeneric.js
@@ -6,5 +6,5 @@ var group__qtypegeneric =
     [ "qTypeGeneric_Rotate", "group__qtypegeneric.html#gad29bd039b0697138bcb05eecab1fae29", null ],
     [ "qTypeGeneric_Set", "group__qtypegeneric.html#ga1a01bce1922bbfb580323e72a5cc0a57", null ],
     [ "qTypeGeneric_Sort", "group__qtypegeneric.html#ga83693af97ce728270f657d818c980dc5", null ],
-    [ "qTypeGeneric_Swap", "group__qtypegeneric.html#ga0e38aca8648c9d20c08d6fd90a4d7a0f", null ]
+    [ "qTypeGeneric_Swap", "group__qtypegeneric.html#gac7191c93e5a62520335e9f55ebd7f5fa", null ]
 ];
