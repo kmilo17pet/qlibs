74f5db153bb6dcb3a28557dbc859f47475b208da
kmilo17pet <kmilo17pet@users.noreply.github.com>
2023-03-07 14:12:59 +0000

    deploy: 05191da170229a99941bd49830026cdefaadde37

--- search/groups_0.js
+++ search/groups_0.js
@@ -2,15 +2,14 @@ var searchData=
 [
   ['qbitfield_0',['qBitField',['../group__qbitfield.html',1,'']]],
   ['qcrc_1',['qCRC',['../group__qcrc.html',1,'']]],
-  ['qffmath_2',['qFFMath',['../group__qffmath.html',1,'']]],
-  ['qfis_3',['qFIS',['../group__qfis.html',1,'']]],
-  ['qfmathex_4',['qFMathEx',['../group__qfmathex.html',1,'']]],
-  ['qfp16_5',['qFP16',['../group__qfp16.html',1,'']]],
-  ['qfvector_6',['qFVector',['../group__qvfloat.html',1,'']]],
-  ['qltisys_7',['qLTISys',['../group__qltisys.html',1,'']]],
-  ['qpid_8',['qPID',['../group__qpid.html',1,'']]],
-  ['qrms_9',['qRMS',['../group__qrms.html',1,'']]],
-  ['qssmoother_10',['qSSMoother',['../group__qssmoother.html',1,'']]],
-  ['qtdl_11',['qTDL',['../group__qtdl.html',1,'']]],
-  ['qtypegeneric_12',['qTypeGeneric',['../group__qtypegeneric.html',1,'']]]
+  ['qfis_2',['qFIS',['../group__qfis.html',1,'']]],
+  ['qfmathex_3',['qFMathEx',['../group__qfmathex.html',1,'']]],
+  ['qfp16_4',['qFP16',['../group__qfp16.html',1,'']]],
+  ['qfvector_5',['qFVector',['../group__qvfloat.html',1,'']]],
+  ['qltisys_6',['qLTISys',['../group__qltisys.html',1,'']]],
+  ['qpid_7',['qPID',['../group__qpid.html',1,'']]],
+  ['qrms_8',['qRMS',['../group__qrms.html',1,'']]],
+  ['qssmoother_9',['qSSMoother',['../group__qssmoother.html',1,'']]],
+  ['qtdl_10',['qTDL',['../group__qtdl.html',1,'']]],
+  ['qtypegeneric_11',['qTypeGeneric',['../group__qtypegeneric.html',1,'']]]
 ];
