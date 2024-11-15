9eac523069e5297db725e6c37fff2813be00ca74
camilo <juancamilo.gomezcadavid@sensiaglobal.com>
2024-11-15 11:55:03 -0500

    bufix on rieman_zeta_product

--- qffmath.c
+++ qffmath.c
@@ -2107,10 +2107,9 @@ static float riemann_zeta_product( float s )
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
         71, 73, 79, 83, 89, 97, 101, 103, 107, 109
     };
-    static const size_t num_primes = sizeof(prime)/sizeof(float);
     float zeta = 1.0F;
 
-    for ( size_t i = 0U ; i < num_primes; ++i ) {
+    for ( size_t i = 0U ; i < sizeof(prime) ; ++i ) {
         const float f = 1.0F - qFFMath_Pow( (float)prime[ i ], -s );
 
         zeta *= f;
